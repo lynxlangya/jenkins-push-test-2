fd02a92e7ac6ef7517371d6ba16e1fcc9268aeb4
wangdaoo <wangdaoo@yeah.net>
2021-10-04 13:24:12 +0800

    feat: jenkins 脚本构建成功！:tada: :tada: :tada:

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <h1>第二个项目测试</h1>
   <h2>服务器测试!!!</h2>
-  <h2>服务器测试!!!</h2>
+  <h2>脚本构建测试成功</h2>
+  <h1 style="color: red">SUCCESS!</h1>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
